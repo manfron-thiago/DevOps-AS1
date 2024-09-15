a69445a2f9b80182e61981655d54c6bf1233029a
Thiago <thiago.mfrn@gmail.com>
2024-09-15 18:43:13 -0300

    Add dockerfile ao projeto

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Faz com que o Vite sirva a aplicação em todas as interfaces de rede
+    port: 3000, // Define a porta 3000 (ou outra, se preferir)
+  },
 })
