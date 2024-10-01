2ff5ce3aaa481531922d2e388d3af31d10928af7
Thiago <thiago.mfrn@gmail.com>
2024-09-30 21:57:38 -0300

    atualizacao testes unitarios

--- src/MyComponent.test.jsx
+++ src/MyComponent.test.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable no-undef */
 
 import { render, screen, fireEvent } from '@testing-library/react';
-import MyComponent from './MyComponent';
 import MyComponent from './MyComponent.jsx';
 
 
